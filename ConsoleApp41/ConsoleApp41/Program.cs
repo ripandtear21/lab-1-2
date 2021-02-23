8cd6025d499d8f5768eeead716e4b168de5ad9ee
ripandtear21 <norman228337@gmail.com>
2021-02-23 09:26:41 +0200

    1

---
+++ ConsoleApp41/ConsoleApp41/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp41
+{
+    class Program
+    {
+        delegate float NumHandler(float num1, float num2, float num3);
+        static void Main(string[] args)
+        {
+            float x = float.Parse(Console.ReadLine());
+            float y = float.Parse(Console.ReadLine());
+            float z = float.Parse(Console.ReadLine());
+            NumHandler numHandler = delegate (float x, float y, float z)
+            {
+                return (x + y + z) / 3;
+            };
+            float d = numHandler(x, y, z);
+            Console.WriteLine(d);
+            Console.Read();
+        }
+        
+    }
+}
